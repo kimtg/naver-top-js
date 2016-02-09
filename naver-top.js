f8b52cd3c1a8a962ad7d4a4f479b21669e1c5d94
KIM Taegyoon <steloflute@gmail.com>
2016-02-10 02:06:54 +0900

    simplified

--- naver-top.js
+++ naver-top.js
@@ -1,32 +1,9 @@
 function getText(strURL)
 {
-    var strResult;
-    
-    try
-    {
-        // Create the WinHTTPRequest ActiveX Object.
-        var WinHttpReq = new ActiveXObject(
-                                  "WinHttp.WinHttpRequest.5.1");
-        
-        //  Create an HTTP request.
-        var temp = WinHttpReq.Open("GET", strURL, false);
-
-        //  Send the HTTP request.
-        WinHttpReq.Send();
-        
-        //  Retrieve the response text.
-        strResult = WinHttpReq.ResponseText;
-    }
-    catch (objError)
-    {
-        strResult = objError + "\n"
-        strResult += "WinHTTP returned error: " + 
-            (objError.number & 0xFFFF).toString() + "\n\n";
-        strResult += objError.description;
-    }
-    
-    //  Return the response text.
-    return strResult;
+  var req = new ActiveXObject("WinHttp.WinHttpRequest.5.1");
+  req.Open("GET", strURL, false);
+  req.Send();
+  return req.ResponseText;
 }
 
 function list_naver() {
